41ad8b7e08e7dd9e7b2aa230f0bc9d431f687956
ShawnLiweiLuo <luolw8026@gmail.com>
2018-10-14 23:34:02 -0700

    Push

--- test.py
+++ test.py
@@ -39,48 +39,51 @@ label_dict = {
 
 improve_model = False
 
-# Load the train and validation data lists
-train_list = np.load('train.npy')
-valid_list = np.load('valid.npy')
+def main():
+    # Load the train and validation data lists
+    train_list = np.load('train.npy')
+    valid_list = np.load('valid.npy')
 
-# Create a dataset using the data list you prepared
-input_dim=(300, 300)
-train_dataset = CityScapeDataset(train_list, input_dim=input_dim, mode='train')
-valid_dataset = CityScapeDataset(valid_list, input_dim=input_dim, mode='test')
+    # Create a dataset using the data list you prepared
+    input_dim=(300, 300)
+    train_dataset = CityScapeDataset(train_list, input_dim=input_dim, mode='train')
+    valid_dataset = CityScapeDataset(valid_list, input_dim=input_dim, mode='test')
 
-# Create a solver to train the model
-solver = Solver(train_dataset, valid_dataset, batch_size=64)
+    # Create a solver to train the model
+    solver = Solver(train_dataset, valid_dataset, batch_size=64)
 
-# Instantiate the model
-num_classes = len(set(label_dict.values())) + 1
-ssd_net = SSD(num_classes).cuda()
+    # Instantiate the model
+    num_classes = len(set(label_dict.values())) + 1
+    ssd_net = SSD(num_classes).cuda()
 
-if improve_model:
-    net_state = torch.load('vehicle_detection/ssd_net')
-    ssd_net.load_state_dict(net_state)
+    if improve_model:
+        net_state = torch.load('vehicle_detection/ssd_net')
+        ssd_net.load_state_dict(net_state)
 
-# Create an optimizer
-optimizer = torch.optim.Adam(ssd_net.parameters(), lr=1e-3)
+    # Create an optimizer
+    optimizer = torch.optim.Adam(ssd_net.parameters(), lr=1e-3)
 
-# Initialize loss function
-loss_function = MultiboxLoss()
+    # Initialize loss function
+    loss_function = MultiboxLoss()
 
-train_bbox_losses = []
-train_class_losses = []
+    train_bbox_losses = []
+    train_class_losses = []
 
-# Training the model
-lr = 1e-3
-for num_epochs in [15, 20, 20, 10, 5]:
-    optimizer.param_groups[0]['lr'] = lr
-    tclass_loss, tbbox_loss, vclass_loss, vbbox_loss = solver.train(ssd_net,
-                                                                    optimizer, 
-                                                                    loss_function,
-                                                                    num_epochs=num_epochs,
-                                                                    print_every=200)
-    train_class_losses += tclass_loss
-    train_bbox_losses += tbbox_loss
-    lr *= 1e-1
+    # Training the model
+    lr = 1e-3
+    for num_epochs in [15, 20, 20, 10, 5]:
+        optimizer.param_groups[0]['lr'] = lr
+        tclass_loss, tbbox_loss, vclass_loss, vbbox_loss = solver.train(ssd_net,
+                                                                        optimizer,
+                                                                        loss_function,
+                                                                        num_epochs=num_epochs,
+                                                                        print_every=200)
+        train_class_losses += tclass_loss
+        train_bbox_losses += tbbox_loss
+        lr *= 1e-1
 
 
+if __name__ == "__main__":
+    main()
 
 
